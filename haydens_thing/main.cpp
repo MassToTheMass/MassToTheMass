c6971e6f6882b0d6145687fedb39574e30163a5a
25bperry <bdawson2007@yahoo.com>
2024-11-20 17:38:46 +0000

    Trying to make the options more readable

--- haydens_thing/main.cpp
+++ haydens_thing/main.cpp
@@ -2,30 +2,22 @@
 #include "meet_storage.hpp"
 #include "event.hpp"
 #include "athlete.hpp"
+#include <algorithm>
 
 int main(){
     //initialize storage system
     MeetStore meets;
     std::string choices;
-    char choice;
-    while(true) { //options to operate the program
-
+    while(true) { //options to operate the prograM
         std::cout << "What would you like to perform:" << std::endl
-                  << "c: Create meet" << std::endl 
-                  << "M: Display meet" << std::endl
-                  << "a: Display all meets" << std::endl
-                  << "A: Display athlete" << std::endl
-                  << "D: Display all athletes" << std::endl
-                  << "q: Quit Program :(" << std::endl;
+                  << "Create: Create meet" << std::endl 
+                  << "Meet: Display meet" << std::endl
+                  << "All Meets: Display all meets" << std::endl
+                  << "Athlete : Display athlete" << std::endl
+                  << "All Athletes: Display all athletes" << std::endl
+                  << "Quit: Quit Program :(" << std::endl;
         std::getline(std::cin, choices);
-        if(choices.length() == 1){
-            choice = choices[0];
-        }
-        else{
-            choice = 'x';
-        }
-        if(choice == 'c'){
-
+        if(choices == 'create'){ //create a meet
             std::string mn, date, file, l;
             std::cout << "Enter the meet name with no spaces: " << std::endl;
             std::cin >> mn; 
@@ -38,7 +30,7 @@ int main(){
             meets.create(l, mn, date, file);
         }
 
-        else if(choice == 'M'){ //display specific meet
+        else if(choices == 'Meet'){ //display specific meet
 
             std::string meetName;
             std::cout << "Enter meet name: " <<std::endl;
@@ -47,11 +39,11 @@ int main(){
             meets.displayEvent(meetName);
         }
 
-        else if(choice == 'a'){ //display all meets
+        else if(choices == 'All Meets'){ //display all meets
             meets.displayAll();
         }
 
-        else if(choice == 'A'){ // display a specific athlete within a specific meet
+        else if(choices == 'Athlete'){ // display a specific athlete within a specific meet
             std::string meetName, athleteName;
             std::cout << "What is the meet name: " << std::endl;
             std::cin >> meetName;
@@ -61,16 +53,13 @@ int main(){
 
         }
 
-        else if(choice == 'D'){ //display every athlete in every meet systematically for results comparison by standardized time
+        else if(choices == 'All Athletes'){ //display every athlete in every meet systematically for results comparison by standardized time
             meets.displayAllAthletes();
         }
 
-        else if(choice == 'q') { //exit the program
+        else if(choices == 'Quit') { //exit the program
             break;
         }
-
-        
-
         else{ //handles incorrect user input here
             std::cout << "Not a choice!" << std::endl;
         }
