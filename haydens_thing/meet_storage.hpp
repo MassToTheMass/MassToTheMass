daa87f078ebe9be6ba09f56f4e63f5e0d0fae398
Mason Christopherson <masonxiv.14@icloud.com>
2024-12-03 16:42:32 +0000

    run?

--- haydens_thing/meet_storage.hpp
+++ haydens_thing/meet_storage.hpp
@@ -59,7 +59,7 @@ public:
 
     void displayEvent(const std::string& eventName) { //display more in depth analysis of a specific event
         if(events.count(eventName)) {
-            events.at(eventName).printStats();
+            events.at(eventName).printStats(events.at(eventName).results);
         } else {
             std::cout << "This meet does not exist" << std::endl; 
         }
