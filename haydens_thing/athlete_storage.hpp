885150c9c856f7c4952f5c730b30bffc1bc13ea7
Mason Christopherson <masonxiv.14@icloud.com>
2024-12-05 15:57:14 +0000

    athlete we iterate

--- haydens_thing/athlete_storage.hpp
+++ haydens_thing/athlete_storage.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <map>
 #include "athlete.hpp"
+#include <fstream>
+#include <iostream>
 
 
 class AthleteStore {
@@ -12,7 +14,13 @@ class AthleteStore {
         AthleteStore() {
             // algorithm for creating unique athletes goes in this constructor
             
+            std::ifstream file_map("file_map.txt");
+            std::string line;
             
+            while (std::getline(file_map, line)) {
+                std::cout << line << std::endl;
+                
+            }
 
         }
-}
+};
